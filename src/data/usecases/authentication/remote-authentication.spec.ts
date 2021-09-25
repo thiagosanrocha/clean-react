c0693ce2074b25f789b450fc5f9306456823936b
Thiago Rocha <thiagorocha010@gmail.com>
2021-09-25 20:59:18 -0300

    test: ensure RemoteAuthentication calls HttpPostClient with correct payload

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,15 +1,20 @@
 import { RemoteAuthentication } from './remote-authentication'
 import { HttpPostClient, HttpResponse, HttpStatusCode, HttpPostParams } from '@data/protocols/http'
+import { AuthCredentials } from '@domain/usecases'
+import { AccountModel } from '@domain/models'
 import faker from 'faker'
 
 class HttpPostClientSpy implements HttpPostClient {
   url?: string
+  payload?: any
   response: HttpResponse = {
     statusCode: HttpStatusCode.ok
   }
 
   async post (params: HttpPostParams): Promise<HttpResponse> {
     this.url = params.url
+    this.payload = params.payload
+
     return Promise.resolve(this.response)
   }
 }
@@ -29,42 +34,53 @@ const makeSut = (url = faker.internet.url()): SutTypes => {
   }
 }
 
+const mockAuthCredentials = (): AuthCredentials => ({
+  email: faker.internet.email(),
+  password: faker.internet.password()
+})
+
+const mockAccountModel = (): AccountModel => ({
+  accessToken: faker.datatype.uuid(),
+  name: faker.name.findName()
+})
+
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
     const url = faker.internet.url()
 
     const { sut, httpPostClientSpy } = makeSut(url)
 
-    const authCredentials = {
-      email: faker.internet.email(),
-      password: faker.internet.password()
-    }
+    const authCredentials = mockAuthCredentials()
 
     await sut.auth(authCredentials)
 
     expect(httpPostClientSpy.url).toBe(url)
   })
 
+  test('Sould call HttpPostClient with correct payload', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+
+    const authCredentials = mockAuthCredentials()
+
+    await sut.auth(authCredentials)
+
+    expect(httpPostClientSpy.payload).toEqual(authCredentials)
+  })
+
   test('Should return an AccountModel if HttpPostClient return 200', async () => {
     const { sut, httpPostClientSpy } = makeSut()
 
-    const mockHttpResponse = {
-      accessToken: faker.datatype.uuid(),
-      name: faker.name.findName()
-    }
+    const accountModel = mockAccountModel()
 
     httpPostClientSpy.response = {
       statusCode: HttpStatusCode.ok,
-      body: mockHttpResponse
+      body: accountModel
     }
 
-    const authCredentials = {
-      email: faker.internet.email(),
-      password: faker.internet.password()
-    }
+    const authCredentials = mockAuthCredentials()
 
     const account = await sut.auth(authCredentials)
 
-    expect(account).toEqual(mockHttpResponse)
+    expect(account).toEqual(accountModel)
   })
 })
